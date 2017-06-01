96ea6c2e6cccc7bdb833c3db5595f89e57e73f8a
John-Marc Chandonia <JMChandonia@lbl.gov>
2017-06-01 21:26:41 +0000

    Fixed test to point to new classpath

--- test/src/genericsutil/test/GenericsUtilServerTest.java
+++ test/src/genericsutil/test/GenericsUtilServerTest.java
@@ -1,6 +1,6 @@
 package genericsutil.test;
 
-import genericsutil.*;
+import us.kbase.genericsutil.*;
 
 import java.io.File;
 import java.net.URL;
@@ -18,7 +18,7 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import genericsutil.GenericsUtilServer;
+import us.kbase.genericsutil.GenericsUtilServer;
 import us.kbase.auth.AuthConfig;
 import us.kbase.auth.AuthToken;
 import us.kbase.auth.ConfigurableAuthService;
@@ -94,7 +94,7 @@ public class GenericsUtilServerTest {
     @Test
     public void testImportGrowthPreMapped() throws Exception {
         ImportCSVParams params = new ImportCSVParams()
-            .withFile(new genericsutil.File().withPath("/kb/module/test/data/20160823-MT123_c_source2_generic.csv"))
+            .withFile(new us.kbase.genericsutil.File().withPath("/kb/module/test/data/20160823-MT123_c_source2_generic.csv"))
             .withObjectName("20160823-MT123_c_source2")
             .withObjectType("KBaseGenerics.NDArray")
             .withWorkspaceName("jmc:1480966800200");
@@ -109,7 +109,7 @@ public class GenericsUtilServerTest {
     // @Test
     public void testImportGrowth() throws Exception {
         ImportCSVParams params = new ImportCSVParams()
-            .withFile(new genericsutil.File().withPath("/kb/module/test/data/growth_nitrate_multi.csv"))
+            .withFile(new us.kbase.genericsutil.File().withPath("/kb/module/test/data/growth_nitrate_multi.csv"))
             .withObjectName("nitrate_growth")
             .withObjectType("KBaseGenerics.NDArray")
             .withWorkspaceName("jmc:1480966800200");
@@ -124,7 +124,7 @@ public class GenericsUtilServerTest {
     // @Test
     public void testImportGrowth2() throws Exception {
         ImportCSVParams params = new ImportCSVParams()
-            .withFile(new genericsutil.File().withPath("/kb/module/test/data/growth_nitrate_simple.csv"))
+            .withFile(new us.kbase.genericsutil.File().withPath("/kb/module/test/data/growth_nitrate_simple.csv"))
             .withObjectName("nitrate_growth")
             .withObjectType("KBaseGenerics.NDArray")
             .withWorkspaceName("jmc:1480966800200");
@@ -203,7 +203,7 @@ public class GenericsUtilServerTest {
     // @Test
     public void testImportFitness() throws Exception {
         ImportCSVParams params = new ImportCSVParams()
-            .withFile(new genericsutil.File().withPath("/kb/module/test/data/fitness_jw710_random.csv"))
+            .withFile(new us.kbase.genericsutil.File().withPath("/kb/module/test/data/fitness_jw710_random.csv"))
             .withObjectName("fitness_jw710")
             .withObjectType("KBaseGenerics.NDArray")
             .withWorkspaceName("jmc:1480966800200");
@@ -218,7 +218,7 @@ public class GenericsUtilServerTest {
     // @Test
     public void testImportFitness2() throws Exception {
         ImportCSVParams params = new ImportCSVParams()
-            .withFile(new genericsutil.File().withPath("/kb/module/test/data/fitness_jw710_random_small.csv"))
+            .withFile(new us.kbase.genericsutil.File().withPath("/kb/module/test/data/fitness_jw710_random_small.csv"))
             .withObjectName("fitness_jw710_small")
             .withObjectType("KBaseGenerics.NDArray")
             .withWorkspaceName("jmc:1480966800200");
@@ -233,7 +233,7 @@ public class GenericsUtilServerTest {
     // @Test
     public void testImportFitness3() throws Exception {
         ImportCSVParams params = new ImportCSVParams()
-            .withFile(new genericsutil.File().withPath("/kb/module/test/data/fitness_multi_random.csv"))
+            .withFile(new us.kbase.genericsutil.File().withPath("/kb/module/test/data/fitness_multi_random.csv"))
             .withObjectName("fitness_multiple_dvh_strains")
             .withObjectType("KBaseGenerics.NDArray")
             .withWorkspaceName("jmc:1480966800200");
@@ -248,7 +248,7 @@ public class GenericsUtilServerTest {
     // @Test
     public void testImportEA() throws Exception {
         ImportCSVParams params = new ImportCSVParams()
-            .withFile(new genericsutil.File().withPath("/kb/module/test/data/activity_single.csv"))
+            .withFile(new us.kbase.genericsutil.File().withPath("/kb/module/test/data/activity_single.csv"))
             .withObjectName("enzyme_activity_dvh")
             .withObjectType("KBaseGenerics.NDArray")
             .withWorkspaceName("jmc:1480966800200");
@@ -263,7 +263,7 @@ public class GenericsUtilServerTest {
     // @Test
     public void testImportEA2() throws Exception {
         ImportCSVParams params = new ImportCSVParams()
-            .withFile(new genericsutil.File().withPath("/kb/module/test/data/activity_replicates.csv"))
+            .withFile(new us.kbase.genericsutil.File().withPath("/kb/module/test/data/activity_replicates.csv"))
             .withObjectName("enzyme_activity_dvh_replicates")
             .withObjectType("KBaseGenerics.NDArray")
             .withWorkspaceName("jmc:1480966800200");
@@ -278,7 +278,7 @@ public class GenericsUtilServerTest {
     // @Test
     public void testImportEA3() throws Exception {
         ImportCSVParams params = new ImportCSVParams()
-            .withFile(new genericsutil.File().withPath("/kb/module/test/data/activity_statistics.csv"))
+            .withFile(new us.kbase.genericsutil.File().withPath("/kb/module/test/data/activity_statistics.csv"))
             .withObjectName("enzyme_activity_dvh_stats")
             .withObjectType("KBaseGenerics.NDArray")
             .withWorkspaceName("jmc:1480966800200");
@@ -293,7 +293,7 @@ public class GenericsUtilServerTest {
     // @Test    
     public void testImportTax() throws Exception {
         ImportCSVParams params = new ImportCSVParams()
-            .withFile(new genericsutil.File().withPath("/kb/module/test/data/otu_abundance.csv"))
+            .withFile(new us.kbase.genericsutil.File().withPath("/kb/module/test/data/otu_abundance.csv"))
             .withObjectName("taxonomic_abundance")
             .withObjectType("KBaseGenerics.NDArray")
             .withWorkspaceName("jmc:1480966800200");
@@ -308,7 +308,7 @@ public class GenericsUtilServerTest {
     // @Test
     public void testImportTax2() throws Exception {
         ImportCSVParams params = new ImportCSVParams()
-            .withFile(new genericsutil.File().withPath("/kb/module/test/data/otu_abundance_multiwell.csv"))
+            .withFile(new us.kbase.genericsutil.File().withPath("/kb/module/test/data/otu_abundance_multiwell.csv"))
             .withObjectName("taxonomic_abundance_multiwell")
             .withObjectType("KBaseGenerics.NDArray")
             .withWorkspaceName("jmc:1480966800200");
@@ -323,7 +323,7 @@ public class GenericsUtilServerTest {
     // @Test
     public void testImportTax3() throws Exception {
         ImportCSVParams params = new ImportCSVParams()
-            .withFile(new genericsutil.File().withPath("/kb/module/test/data/otu_abundance_timeseries.csv"))
+            .withFile(new us.kbase.genericsutil.File().withPath("/kb/module/test/data/otu_abundance_timeseries.csv"))
             .withObjectName("taxonomic_abundance_timeseries")
             .withObjectType("KBaseGenerics.NDArray")
             .withWorkspaceName("jmc:1480966800200");
@@ -338,7 +338,7 @@ public class GenericsUtilServerTest {
     @Test
     public void testImportEP() throws Exception {
         ImportCSVParams params = new ImportCSVParams()
-            .withFile(new genericsutil.File().withPath("/kb/module/test/data/heterogenous_sampling.csv"))
+            .withFile(new us.kbase.genericsutil.File().withPath("/kb/module/test/data/heterogenous_sampling.csv"))
             .withObjectName("heterogenous_sampling")
             .withObjectType("KBaseGenerics.HNDArray")
             .withWorkspaceName("jmc:1480966800200");
@@ -353,7 +353,7 @@ public class GenericsUtilServerTest {
     // @Test(expected=Exception.class)
     public void testBad1() throws Exception {
         ImportCSVParams params = new ImportCSVParams()
-            .withFile(new genericsutil.File().withPath("/kb/module/test/data/growth_typed_bad1.csv"))
+            .withFile(new us.kbase.genericsutil.File().withPath("/kb/module/test/data/growth_typed_bad1.csv"))
             .withObjectName("bad_growth_data_1")
             .withObjectType("KBaseGenerics.NDArray")
             .withWorkspaceName("jmc:1480966800200");
@@ -367,7 +367,7 @@ public class GenericsUtilServerTest {
     // @Test(expected=Exception.class)
     public void testBad2() throws Exception {
         ImportCSVParams params = new ImportCSVParams()
-            .withFile(new genericsutil.File().withPath("/kb/module/test/data/growth_typed_bad2.csv"))
+            .withFile(new us.kbase.genericsutil.File().withPath("/kb/module/test/data/growth_typed_bad2.csv"))
             .withObjectName("bad_growth_data_2")
             .withObjectType("KBaseGenerics.NDArray")
             .withWorkspaceName("jmc:1480966800200");
@@ -381,7 +381,7 @@ public class GenericsUtilServerTest {
     // @Test(expected=Exception.class)
     public void testBad3() throws Exception {
         ImportCSVParams params = new ImportCSVParams()
-            .withFile(new genericsutil.File().withPath("/kb/module/test/data/growth_typed_bad3.csv"))
+            .withFile(new us.kbase.genericsutil.File().withPath("/kb/module/test/data/growth_typed_bad3.csv"))
             .withObjectName("bad_growth_data_3")
             .withObjectType("KBaseGenerics.NDArray")
             .withWorkspaceName("jmc:1480966800200");
@@ -395,14 +395,14 @@ public class GenericsUtilServerTest {
     // @Test
     public void testImportDictionaries() throws Exception {
         ImportOBOParams params = new ImportOBOParams()
-            .withFile(new genericsutil.File().withPath("/kb/module/test/data/data_type_ontology.obo"))
+            .withFile(new us.kbase.genericsutil.File().withPath("/kb/module/test/data/data_type_ontology.obo"))
             .withObjectName("data_type_ontology")
             .withWorkspaceName("jmc:1480966800200");
         ImportResult rv = impl.importObo(params, token, (RpcContext)null);
         Assert.assertNotNull(rv);
 
         params = new ImportOBOParams()
-            .withFile(new genericsutil.File().withPath("/kb/module/test/data/context_measurement_ontology.obo"))
+            .withFile(new us.kbase.genericsutil.File().withPath("/kb/module/test/data/context_measurement_ontology.obo"))
             .withObjectName("context_measurement_ontology")
             .withWorkspaceName("jmc:1480966800200");
         rv = impl.importObo(params, token, (RpcContext)null);
@@ -420,14 +420,14 @@ public class GenericsUtilServerTest {
 
         /*
         params = new ImportCSVParams()
-            .withFile(new genericsutil.File().withPath("/kb/module/test/data/growth_typed.csv"))
+            .withFile(new us.kbase.genericsutil.File().withPath("/kb/module/test/data/growth_typed.csv"))
             .withObjectName("m1")
             .withObjectType("KBaseGenerics.Matrix2D")
             .withWorkspaceName("psnovichkov:1480537596117");
         rv = impl.importCsv(params, token, (RpcContext)null);
         Assert.assertNotNull(rv);
         params = new ImportCSVParams()
-            .withFile(new genericsutil.File().withPath("/kb/module/test/data/growth_typed_2.csv"))
+            .withFile(new us.kbase.genericsutil.File().withPath("/kb/module/test/data/growth_typed_2.csv"))
             .withObjectName("m2")
             .withObjectType("KBaseGenerics.Matrix2D")
             .withWorkspaceName("psnovichkov:1480537596117");
@@ -435,35 +435,35 @@ public class GenericsUtilServerTest {
         Assert.assertNotNull(rv);
         */
         params = new ImportCSVParams()
-            .withFile(new genericsutil.File().withPath("/kb/module/test/data/fitness_typed_small.csv"))
+            .withFile(new us.kbase.genericsutil.File().withPath("/kb/module/test/data/fitness_typed_small.csv"))
             .withObjectName("Shewanella_fitness_2D")
             .withObjectType("KBaseGenerics.Matrix2D")
             .withWorkspaceName("psnovichkov:1480537596117");
         rv = impl.importCsv(params, token, (RpcContext)null);
         Assert.assertNotNull(rv);
         params = new ImportCSVParams()
-            .withFile(new genericsutil.File().withPath("/kb/module/test/data/fitness_typed_small.csv"))
+            .withFile(new us.kbase.genericsutil.File().withPath("/kb/module/test/data/fitness_typed_small.csv"))
             .withObjectName("Shewanella_fitness_ND")
             .withObjectType("KBaseGenerics.NDArray")
             .withWorkspaceName("psnovichkov:1480537596117");
         rv = impl.importCsv(params, token, (RpcContext)null);
         Assert.assertNotNull(rv);
         params = new ImportCSVParams()
-            .withFile(new genericsutil.File().withPath("/kb/module/test/data/fitness_typed_small_2.csv"))
+            .withFile(new us.kbase.genericsutil.File().withPath("/kb/module/test/data/fitness_typed_small_2.csv"))
             .withObjectName("Shewanella_fitness_1_condition")
             .withObjectType("KBaseGenerics.Array")
             .withWorkspaceName("psnovichkov:1480537596117");
         rv = impl.importCsv(params, token, (RpcContext)null);
         Assert.assertNotNull(rv);
         params = new ImportCSVParams()
-            .withFile(new genericsutil.File().withPath("/kb/module/test/data/otu_abundance_typed.csv"))
+            .withFile(new us.kbase.genericsutil.File().withPath("/kb/module/test/data/otu_abundance_typed.csv"))
             .withObjectName("Taxonomy_one_sample")
             .withObjectType("KBaseGenerics.Array")
             .withWorkspaceName("psnovichkov:1480537596117");
         rv = impl.importCsv(params, token, (RpcContext)null);
         Assert.assertNotNull(rv);
         params = new ImportCSVParams()
-            .withFile(new genericsutil.File().withPath("/kb/module/test/data/otu_abundance_multiwell_typed.csv"))
+            .withFile(new us.kbase.genericsutil.File().withPath("/kb/module/test/data/otu_abundance_multiwell_typed.csv"))
             .withObjectName("Taxonomy_multiple_samples")
             .withObjectType("KBaseGenerics.Matrix2D")
             .withWorkspaceName("psnovichkov:1480537596117");
